c632749a2496c53025ad0e1e605206b5d79c6ad3
pat <pat.escalona7@gmail.com>
2022-09-13 07:57:57 -0500

    Initial commit
    Failed to set up repo correctly when I started
    this on my laptop.
    Repo set up no and working files are tracked.
    Need to start over on laptop with a git pull.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
+const repeatString = function (number) {
+    //let n = number;
+    const myArray = [];
+    let response = "";
 
+
+    if (number < 0) {
+        return response = 'ERROR';
+    }
+
+    for (let i = 0; i < number; i++) {
+        myArray.push("hey");
+    }
+
+    response = myArray.toString()
+    response = response.replaceAll(",", "");
+    return response;
 };
 
 // Do not edit below this line
