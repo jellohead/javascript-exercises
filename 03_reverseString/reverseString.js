44da71d6af19038b90b4b512e1c7b14aa81d561e
Pat Escalona <pat.escalona7@gmail.com>
2022-09-13 15:21:49 -0500

    Exercise 03 first test working
    reverses string and returns string to calling function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function () {
+    const myString = "hello";
+    const myArray = myString.split("");
+    const myReverseArray = myArray.reverse();
+    let myReverseString = myReverseArray.join("");
+    return myReverseString;
 };
+
+
+// myString.split(""); put string into an array with 1 element at each index
+// myArray.reverse();  reverse order of items in an array
 
 // Do not edit below this line
 module.exports = reverseString;
+
