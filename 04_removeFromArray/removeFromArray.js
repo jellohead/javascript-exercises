4179c91b9f817ce050ca9dac05aaa472e15f0056
Pat Escalona <pat.escalona7@gmail.com>
2022-09-13 22:31:35 -0500

    04 Passsed tests 1 and 2

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+    const myArray = [1, 2, 3, 4];
+    // let myIndex = myArray.indexOf(3);
+    // console.log("myIndex = " + myIndex);
+    // console.log("myArray element at myIndex = " + myArray[myIndex]);
+    // newArray = myArray.splice(myIndex, 1);
 
+    let newArray = myArray.filter(function (value, index, arr) {
+        return (value != 3) && (value != 2);
+    });
+    console.log("newArray = " + newArray);
+    return newArray;
 };
 
 // Do not edit below this line
