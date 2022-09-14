e9e9d10324feec457e03f552877a8535aa8ae90b
Pat Escalona <pat.escalona7@gmail.com>
2022-09-13 22:47:51 -0500

    Problem 04 all tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,12 @@
 const removeFromArray = function () {
-    const myArray = [1, 2, 3, 4];
+    const myArray = [1, 2, 3];
     // let myIndex = myArray.indexOf(3);
     // console.log("myIndex = " + myIndex);
     // console.log("myArray element at myIndex = " + myArray[myIndex]);
     // newArray = myArray.splice(myIndex, 1);
 
     let newArray = myArray.filter(function (value, index, arr) {
-        return (value != 1) && (value != 2) && (value != 3) && (value != 4);
+        return (value !== "1") && (value !== 3);
     });
     console.log("newArray = " + newArray);
     return newArray;
