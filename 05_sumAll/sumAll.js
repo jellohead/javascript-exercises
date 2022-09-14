c401244cf67a4a27a3585a34bf8935c494bc9259
pat <pat.escalona7@gmail.com>
2022-09-14 15:02:55 -0500

    05 passed first 5 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,12 +1,15 @@
 const sumAll = function () {
-    let firstValue = 123;
-    let secondValue = 1;
+    let firstValue = 0;
+    let secondValue = -4;
     let startValue;
     let endValue;
     let i = 0;
     let mySum = 0;
 
-    if (firstValue > secondValue) {
+
+    if (firstValue < 0 || secondValue < 0) {
+        return ('ERROR');
+    } else if (firstValue > secondValue) {
         startValue = secondValue;
         endValue = firstValue;
     }
@@ -28,3 +31,4 @@ module.exports = sumAll;
 // expect(sumAll(1, 4)).toEqual(10);
 // expect(sumAll(1, 4000)).toEqual(8002000);
 // expect(sumAll(123, 1)).toEqual(7626);
+// expect(sumAll(-10, 4)).toEqual('ERROR');
