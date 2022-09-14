c432e0c40ff1b339b6a7ac5a3d11a6f73b60830f
pat <pat.escalona7@gmail.com>
2022-09-14 07:40:59 -0500

    05 Passed first two tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function () {
+    let firstValue = 1;
+    let secondValue = 4000;
+    let startValue;
+    let endValue;
+    let i = 0;
+    let mySum = 0;
 
-};
+    if secondValue > firstValue {
+        startValue = secondValue;
+    }
+
+    for (i = firstValue; i <= secondValue; i++) {
+        mySum += i;
+        console.log(i);
+    }
+
+    console.log(mySum);
+    return mySum;
+
+}
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
+// expect(sumAll(1, 4)).toEqual(10);
+// expect(sumAll(1, 4000)).toEqual(8002000);
+// expect(sumAll(123, 1)).toEqual(7626);
