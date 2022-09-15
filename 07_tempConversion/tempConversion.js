e19da390220365619008f84d4e72a34a917734ad
Pat Escalona <pat.escalona7@gmail.com>
2022-09-14 19:54:32 -0500

    07 all tests passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,15 +1,14 @@
-let tempF = 32;
-let tempC = 0;
+let tempF = -100;
+let tempC = 73.2;
 const ftoc = function () {
-  // let tempF = 32;
-  let tempF = (tempC - 32) * 5 / 9;
-  return tempC;
+  let tempC = (tempF - 32) * 5 / 9;
+  console.log("tempC = " + tempC + ". typeof = " + typeof tempC.toFixed(1));
+  return +tempC.toFixed(1);
 };
 
 const ctof = function () {
-
-  let tempC = tempF * 9 / 5 + 32;
-  return tempF;
+  let tempF = tempC * 9 / 5 + 32;
+  return +tempF.toFixed(1);
 };
 
 // Do not edit below this line
@@ -17,3 +16,8 @@ module.exports = {
   ftoc,
   ctof
 };
+
+// expect(ftoc(-100)).toEqual(-73.3);
+// expect(ctof(0)).toEqual(32);
+// expect(ctof(73.2)).toEqual(163.8);
+// expect(ctof(-10)).toEqual(14);
