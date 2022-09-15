620190677aa6472defd55ec604704d2fa4094d99
Pat Escalona <pat.escalona7@gmail.com>
2022-09-14 19:29:58 -0500

    07 initial commit
    first test passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+let tempF = 32;
+let tempC = 0;
+const ftoc = function () {
+  // let tempF = 32;
+  let tempF = (tempC - 32) * 5 / 9;
+  return tempC;
 };
 
-const ctof = function() {
+const ctof = function () {
 
+  let tempC = tempF * 9 / 5 + 32;
+  return tempF;
 };
 
 // Do not edit below this line
