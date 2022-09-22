9608e0c3a83445bbc255880036f507b5fd458d3c
pat <pat.escalona7@gmail.com>
2022-09-22 07:51:31 -0500

    Works with punctuation, upper, and lower case

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,11 +1,17 @@
 const palindromes = inputWords => {
-    let reverseWords = inputWords.split("").reverse().join("");
-    console.log(reverseWords);
-    return (inputWords === reverseWords ? true : false);
+    let cleanedWords = inputWords.toLowerCase().replace(/[^a-z]/gi, '');
+    console.log('cleanedWords = ' + cleanedWords);
+    let reverseWords = cleanedWords.split("").reverse().join("");
+    console.log('reverseWords = ' + reverseWords);
+    return (cleanedWords === reverseWords ? true : false);
 }
 
-let inputWords = 'racecar';
+let inputWords = 'RaC=eca!*r';
+console.log('inputWords = ' + inputWords);
 console.log(palindromes(inputWords));
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+// expect(palindromes('racecar!')).toBe(true);
