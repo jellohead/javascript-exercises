9d8913329534f60d45a091696e8103d93785d164
pat <pat.escalona7@gmail.com>
2022-09-22 08:47:15 -0500

    10 initial commit
    started working on fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function () {
+    return 3;
 };
+
+let inputNumber = 4;
+console.log(fibonacci(inputNumber));
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+// expect(fibonacci(4)).toBe(3);
+
