c1d3987439c9274d8796dd5363b72a71caad5bfd
pat <pat.escalona7@gmail.com>
2022-09-22 07:36:46 -0500

    09 first test passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
+const palindromes = inputWords => {
+    let reverseWords = inputWords.split("").reverse().join("");
+    console.log(reverseWords);
+    return (inputWords === reverseWords ? true : false);
+}
 
-};
+let inputWords = 'racecar';
+console.log(palindromes(inputWords));
 
 // Do not edit below this line
 module.exports = palindromes;
