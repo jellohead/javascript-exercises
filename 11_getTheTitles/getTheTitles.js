6a024129aa07dad1bd8b1a2823e1baca560b25be
pat <pat.escalona7@gmail.com>
2022-09-22 10:30:39 -0500

    11 Problem complete

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
-const getTheTitles = function() {
+const books = [
+    {
+        title: 'Book',
+        author: 'Name'
+    },
+    {
+        title: 'Book2',
+        author: 'Name2'
+    }
+]
 
+
+const getTheTitles = (books) => {
+    const results = books.map((books) => books.title);
+    return results;
 };
+
+console.log(getTheTitles(books))
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+// expect(getTheTitles(books)).toEqual(['Book', 'Book2']);
