42bf0d840e20b54da80fae339bd68a506912cf73
Pat Escalona <pat.escalona7@gmail.com>
2022-09-26 22:06:56 -0500

    routine to fined oldest not working yet

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,28 @@
-const findTheOldest = function () {
+// sort through records and find the oldest person
 
+const findTheOldest = function (array) {
+  console.log(JSON.stringify(array));
+  return array.reduce((oldestPerson, currentPerson) => {
+    const oldestAge = getAge(array.oldestPerson);
+    const currentAge = getAge(array.currentPerson);
+    return oldestAge > currentAge ? oldestPerson : currentPerson;
+  })
 };
+
+// determine age of person 
+const getAge = function (current) {
+  console.log("getAge current.yearOfDeath = " + current.yearOfDeath);
+
+  if (yearOfDeath) {
+    return (current.yearOfDeath - current.yearOfBirth);
+  } else {
+    return (new Date().getFullYear - current.yearOfBirth);
+  }
+}
+
+
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
