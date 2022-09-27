45f0d501097d2d36c51b47754da2e53afced970f
pat <pat.escalona7@gmail.com>
2022-09-27 06:01:08 -0500

    trying to get repo copies in sync

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function () {
-
+const findTheOldest = function (array) {
+    return array.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge < currentAge ? currentPerson : oldest;
+    })
 };
+
+
+const getAge = function (birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+        console.log(death);
+    }
+    return death - birth;
+};
+
+// calculate the age
+
+// sort ages to return object for oldest person
 
 // Do not edit below this line
 module.exports = findTheOldest;
