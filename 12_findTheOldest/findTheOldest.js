14e677df296269badc3f701168f4fe6455493712
pat <pat.escalona7@gmail.com>
2022-09-27 07:42:39 -0500

    Passed first test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,22 +1,22 @@
 // sort through records and find the oldest person
 
 const findTheOldest = function (array) {
-  console.log(JSON.stringify(array));
+  console.log("findTheOldest array = " + JSON.stringify(array));
   return array.reduce((oldestPerson, currentPerson) => {
-    const oldestAge = getAge(array.oldestPerson);
-    const currentAge = getAge(array.currentPerson);
+    const oldestAge = getAge(oldestPerson.yearOfDeath, oldestPerson.yearOfBirth);
+    const currentAge = getAge(currentPerson.yearOfDeath, currentPerson.yearOfBirth);
     return oldestAge > currentAge ? oldestPerson : currentPerson;
   })
 };
 
 // determine age of person 
-const getAge = function (current) {
-  console.log("getAge current.yearOfDeath = " + current.yearOfDeath);
+const getAge = function (death, birth) {
+  console.log("death " + death + " birth " + birth)
 
-  if (yearOfDeath) {
-    return (current.yearOfDeath - current.yearOfBirth);
+  if (death) {
+    return (death - birth);
   } else {
-    return (new Date().getFullYear - current.yearOfBirth);
+    return (new Date().getFullYear - birth);
   }
 }
 
@@ -26,25 +26,3 @@ const getAge = function (current) {
 
 // Do not edit below this line
 module.exports = findTheOldest;
-
-
-/*
-
-const findTheOldest = function(array) {
-  return array.reduce((oldest, currentPerson) => {
-    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
-    const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
-    return oldestAge < currentAge ? currentPerson : oldest;
-  })
-};
-
-const getAge = function(birth, death) {
-  if (!death) {
-    death = new Date().getFullYear();
-  }
-  return death - birth;
-};
-
-module.exports = findTheOldest;
-
-*/
