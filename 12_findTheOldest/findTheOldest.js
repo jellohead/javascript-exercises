c09f0792acb6ca90d018261a1ad3af98d6da2bb2
pat <pat.escalona7@gmail.com>
2022-09-27 09:03:25 -0500

    12 all test working

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,8 +1,9 @@
 // sort through records and find the oldest person
 
 const findTheOldest = function (array) {
-  console.log("findTheOldest array = " + JSON.stringify(array));
   return array.reduce((oldestPerson, currentPerson) => {
+    console.log("compare " + oldestPerson.name + " to " + currentPerson.name);
+
     const oldestAge = getAge(oldestPerson.yearOfDeath, oldestPerson.yearOfBirth);
     const currentAge = getAge(currentPerson.yearOfDeath, currentPerson.yearOfBirth);
     return oldestAge > currentAge ? oldestPerson : currentPerson;
@@ -13,11 +14,11 @@ const findTheOldest = function (array) {
 const getAge = function (death, birth) {
   console.log("death " + death + " birth " + birth)
 
-  if (death) {
-    return (death - birth);
-  } else {
-    return (new Date().getFullYear - birth);
+  if (typeof death === 'undefined') {
+    death = new Date().getFullYear();
+    console.log("year of death or current year is " + new Date().getFullYear());
   }
+  return death - birth;
 }
 
 
